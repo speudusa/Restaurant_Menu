e1f15d5ef948ccb7f50b0ae9567b5afc2dcc6f06
Courtney Frey <courtneyb.frey@gmail.com>
2022-04-22 14:22:08 -0500

    updated and re-designed

---
+++ Restaurant_Menu/MenuItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Restaurant_Menu
+{
+    public class MenuItem
+    {
+
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+        public MenuItem(double price, string description, string category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+        }
+
+        public string IsItNew()
+        {
+            if (IsNew)
+            {
+                return ("New item!");
+            }
+            else
+            {
+                return ("This is not new.");
+            }
+        }
+
+        public override bool Equals(object comparison)
+        {
+            if (comparison == this)
+            {
+                return true;
+            }
+
+            if (comparison == null)
+            {
+                return false;
+            }
+
+            if (comparison.GetType() != GetType())
+            {
+                return false;
+            }
+
+            MenuItem theItem = comparison as MenuItem;
+            return theItem.Description == Description;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Description);
+        }
+    }
+}
---
+++ Restaurant_Menu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant_Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuItem newItem1 = new MenuItem(4.99, "Garlic linguine", "Dinner", true);
+            MenuItem newItem2 = new MenuItem(6.99, "Really good steak", "Dinner", false);
+            MenuItem newItem3 = new MenuItem(2.99, "Delicious wings", "Appetizer", true);
+
+            DateTime today = DateTime.Now;
+            List<MenuItem> startingMenu = new List<MenuItem>();
+
+            Menu ourMenu = new Menu(today, startingMenu);
+
+            //adding to the menu (initial menu)
+            ourMenu.AddItem(newItem1);
+            ourMenu.AddItem(newItem3);
+            //how is the menu?
+            ourMenu.PrintMenu();
+            Console.WriteLine("\n ------------- \n");
+
+
+            ourMenu.AddItem(newItem2);
+            ourMenu.PrintMenu();
+            Console.WriteLine("\n ------------- \n");
+
+            ourMenu.PrintItem(newItem3);
+            ourMenu.RemoveItem(newItem2);
+            Console.WriteLine("\n ------------- \n");
+
+            ourMenu.PrintMenu();
+
+
+
+
+        }
+    }
+}
